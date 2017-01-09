9b47eb1c96fcd50cf82f40503399b1b583ef33a1
Дмитрий Жур <tehzienergy@gmail.com>
2017-01-09 15:48:43 +0300

    add gulp-watch

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,10 @@ var gulp = require('gulp'),
     rename = require("gulp-rename"),
     concat = require("gulp-concat"),
     insert = require('gulp-insert'),
-    runSequence = require('run-sequence')
+    runSequence = require('run-sequence'),
+    watch = require('gulp-watch'),
+    batch = require('gulp-batch'),
+    plumber = require('gulp-plumber');
 
 gulp.task('default', function () {
     gulp.start('build');
@@ -31,6 +34,7 @@ gulp.task('clean', function () {
 
 gulp.task('images', function() {
   gulp.src(['./src/images/*','./src/images/**/*','./src/blocks/**/images/*'])
+    .pipe(plumber())
     .pipe(rename({dirname: ''}))
     .pipe(gulp.dest('./build/img'))
     .pipe(browserSync.reload({stream:true}));
@@ -39,6 +43,7 @@ gulp.task('images', function() {
 gulp.task('templates', function () {
   var params = {};
   gulp.src('./src/pages/*.pug')
+    .pipe(plumber())
     .pipe(pug({
       locals: params,
       pretty: true
@@ -49,23 +54,27 @@ gulp.task('templates', function () {
 
 gulp.task('fonts', function () {
   gulp.src('./src/fonts/**/*')
+    .pipe(plumber())
     .pipe(gulp.dest('./build/fonts'))
     .pipe(browserSync.reload({stream:true}));
 });
 
 gulp.task('scripts:jquery', function () {
   gulp.src('./src/scripts/jquery.min.js')
+    .pipe(plumber())
     .pipe(gulp.dest('./build/js'));
 });
 
 gulp.task('scripts:plugins', function () {
   gulp.src('./src/scripts/plugins/*.js')
+    .pipe(plumber())
     .pipe(concat('plugins.js'))
     .pipe(gulp.dest('./build/js'));
 });
 
 gulp.task('scripts', function () {
   gulp.src(['./src/blocks/**/*.js', './src/scripts/script.js'])
+    .pipe(plumber())
     .pipe(concat('script.js'))
     .pipe(insert.wrap('$(document).ready(function(){', '})'))
     .pipe(gulp.dest('./build/js'))
@@ -84,6 +93,7 @@ gulp.task('server', function() {
 
 gulp.task('styles', function () {
   gulp.src('./src/styles/style.less')
+    .pipe(plumber())
     .pipe(less())
     .pipe(autoprefixer({
         browsers: ['last 20 versions']
@@ -93,9 +103,24 @@ gulp.task('styles', function () {
 });
 
 gulp.task('watch', function () {
-    gulp.watch(['./src/styles/*.less', './src/styles/global/*.less', './src/blocks/**/*.less', './src/styles/plugins/*.less'], ['styles']); gulp.watch(['./src/pages/*.pug','./src/templates/*.pug','./src/blocks/**/*.pug'], ['templates']);
-    gulp.watch(['./src/images/*','./src/blocks/**/images/*'], ['images']);
-    gulp.watch(['./src/blocks/**/*.js', './src/scripts/*.js'], ['scripts']);
+    watch(['./src/styles/*.less', './src/styles/global/*.less', './src/blocks/**/*.less', './src/styles/plugins/*.less'], batch(function (events, done) {
+        gulp.start('styles', done);
+    }));
+    watch(['./src/pages/*.pug','./src/templates/*.pug','./src/blocks/**/*.pug'], batch(function (events, done) {
+        gulp.start('templates', done);
+    }));
+    watch(['./src/images/*','./src/blocks/**/images/*'], batch(function (events, done) {
+        gulp.start('images', done);
+    }));
+    watch(['./src/blocks/**/*.js', './src/scripts/*.js'], batch(function (events, done) {
+        gulp.start('scripts', done);
+    }));
+    watch('./src/fonts/*', batch(function (events, done) {
+        gulp.start('fonts', done);
+    }));
+    watch('./src/scripts/plugins/*.js', batch(function (events, done) {
+        gulp.start('scripts:plugins', done);
+    }));
 });
 
-gulp.task('dev', [ 'clean', 'build', 'server', 'watch']);
+gulp.task('dev', ['build', 'server', 'watch']);
